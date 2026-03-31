83effe2f22a60e15ff614d406e9eb786ea79c2a5
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2026-03-31 15:40:05 +0700

    Kirim file latihan-14

--- gaskoding-js-latihan-14/latihan-14.js
+++ gaskoding-js-latihan-14/latihan-14.js
@@ -1,6 +1,6 @@
 /**
  * Buatkan 1 buah fungsi JumlahKata dan 1 buah parameter input,
- * Diberikan sebuah string, contoh "Hello World atau Halo Dunia",
+ * Diberikan sebuah string, contoh "Hello World",
  * Hitunglah jumlah kata yang terdapat pada string tersebut.
  * Hello World atau Halo Dunia! = 5 kata
  * 012345678910
